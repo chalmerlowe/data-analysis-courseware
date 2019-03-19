4696fde494ad36b02fae1c1ee2e369642bbef911
Chalmer Lowe <chalmer.lowe@gmail.com>
2019-03-18 20:43:43 -1000

    updated the dictionary lesson

---
+++ 07_dicts/my_dictionary.py
@@ -0,0 +1,10 @@
+d = {'name': 'dark lord',
+     'age': 42,
+     'favorite_foods': ['pizza', 'sushi', 'ramen'] ,
+     'favorite_songs': ['crazy train', 'heathens'],
+    }
+
+d['fav_ice_cream'] = 'butter pecan'
+d['fav_cookie'] = 'chocolate chip with walnuts'
+
+print(d)
