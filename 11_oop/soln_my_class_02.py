94ed7c5b396468f54f1b8fb8fe093afdeff005be
nbgitpuller <nbgitpuller@example.com>
2019-03-28 07:45:53 +0000

    added oop lesson

---
+++ 11_oop/soln_my_class_02.py
@@ -0,0 +1,19 @@
+# * Create a class called `Car`
+# * Define an `__init__()` method that takes a two parameters and assigns them to `color` and `mpg` (miles per gallon) attributes respectively
+# * Define a method called `set_color` that takes in a parameter and assigns that to `color`
+# * Define a method called `print_specs` that prints the values associatd with `color` and `mpg`
+# * Create an object based on your class.
+
+class Car:
+    def __init__(self, color, mpg):
+        self.color = color
+        self.mpg = mpg
+        
+    def set_color(self, color):
+        self.color = color
+    
+    def print_specs(self):
+        print('Your car is {} and has a mpg of: {}'.format(self.color, self.mpg))
+
+sportscar = Car('red', 42)        
+        
