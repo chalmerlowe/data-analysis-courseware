20a4dd4aedab00aeb19a6af68326936100c8fd2f
nbgitpuller <nbgitpuller@example.com>
2019-03-28 04:58:57 +0000

    removed unneeded files

--- 09_files_csv/folder/soln_my_files_03.py
+++
@@ -1,9 +0,0 @@
-fin = open('log_file_1000.csv')
-
-count = 0
-for line in fin:
-    count += 1
-    if 'SELINA' in line:
-        print('Corrupt line:', count, line)
-        
-print('Total lines:', count)
